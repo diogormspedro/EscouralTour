de2bb830df11a9aee527adf9943e2b80628b385f
diogormspedro <diogormspedro@gmail.com>
2026-02-28 00:15:14 +0000

    Initial commit

---
+++ data.js
@@ -0,0 +1,759 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gruta-escoural--setup-001",
+      "name": "Gruta Escoural- Setup 001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.184966616787694,
+        "pitch": -0.06033533247200751,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.175263600002804,
+          "pitch": 0.2419050470489168,
+          "rotation": 0,
+          "target": "1-gruta-escoural--setup-002"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-gruta-escoural--setup-002",
+      "name": "Gruta Escoural- Setup 002",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.947471694022207,
+        "pitch": 0.07378356922781393,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1223952986383523,
+          "pitch": 0.27237361676459315,
+          "rotation": 0,
+          "target": "0-gruta-escoural--setup-001"
+        },
+        {
+          "yaw": 0.859877809779217,
+          "pitch": 0.24991759445819994,
+          "rotation": 0,
+          "target": "3-gruta-escoural--setup-004"
+        },
+        {
+          "yaw": 1.4757944095481283,
+          "pitch": 0.2532957697784912,
+          "rotation": 0,
+          "target": "2-gruta-escoural--setup-003"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-gruta-escoural--setup-003",
+      "name": "Gruta Escoural- Setup 003",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.46293088390749304,
+          "pitch": 0.22056325232435015,
+          "rotation": 0,
+          "target": "1-gruta-escoural--setup-002"
+        },
+        {
+          "yaw": -0.0339374276585751,
+          "pitch": 0.20466248890352645,
+          "rotation": 0,
+          "target": "3-gruta-escoural--setup-004"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-gruta-escoural--setup-004",
+      "name": "Gruta Escoural- Setup 004",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.6332591484171006,
+        "pitch": 0.21444485637640298,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3945129239623064,
+          "pitch": 0.3269849280776018,
+          "rotation": 0,
+          "target": "1-gruta-escoural--setup-002"
+        },
+        {
+          "yaw": -1.8748373376753058,
+          "pitch": 0.3140199670240875,
+          "rotation": 0,
+          "target": "2-gruta-escoural--setup-003"
+        },
+        {
+          "yaw": 2.3856140532986077,
+          "pitch": 0.16521035110039684,
+          "rotation": 0,
+          "target": "4-gruta-escoural--setup-006"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-gruta-escoural--setup-006",
+      "name": "Gruta Escoural- Setup 006",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09661450300521857,
+          "pitch": -0.027998202900521463,
+          "rotation": 0,
+          "target": "3-gruta-escoural--setup-004"
+        },
+        {
+          "yaw": 2.38650173805756,
+          "pitch": 0.8837295963312268,
+          "rotation": 0,
+          "target": "5-gruta-escoural--setup-007"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-gruta-escoural--setup-007",
+      "name": "Gruta Escoural- Setup 007",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.521923368474015,
+          "pitch": -0.05172083376239911,
+          "rotation": 0,
+          "target": "4-gruta-escoural--setup-006"
+        },
+        {
+          "yaw": -3.0677511743597456,
+          "pitch": 0.5330402590618704,
+          "rotation": 0,
+          "target": "6-gruta-escoural--setup-008"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-gruta-escoural--setup-008",
+      "name": "Gruta Escoural- Setup 008",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.3195910537821085,
+        "pitch": 0.7653568061871514,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.773689936979957,
+          "pitch": 0.15301617492941588,
+          "rotation": 0,
+          "target": "5-gruta-escoural--setup-007"
+        },
+        {
+          "yaw": -1.6643599478967896,
+          "pitch": 0.8464810742428952,
+          "rotation": 0,
+          "target": "7-gruta-escoural--setup-009"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-gruta-escoural--setup-009",
+      "name": "Gruta Escoural- Setup 009",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8335194619627844,
+        "pitch": 0.524281062668674,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4586959643479904,
+          "pitch": 0.4248895193871398,
+          "rotation": 0,
+          "target": "8-gruta-escoural--setup-010"
+        },
+        {
+          "yaw": -1.6215949537182794,
+          "pitch": 0.14536357402749722,
+          "rotation": 0,
+          "target": "6-gruta-escoural--setup-008"
+        },
+        {
+          "yaw": 0.7130371509551416,
+          "pitch": 0.6242569135952927,
+          "rotation": 0,
+          "target": "11-gruta-escoural--setup-016"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-gruta-escoural--setup-010",
+      "name": "Gruta Escoural- Setup 010",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.2256465947651716,
+        "pitch": -0.07105757529085821,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.212608825414925,
+          "pitch": 0.17625989995555358,
+          "rotation": 0,
+          "target": "9-gruta-escoural--setup-011"
+        },
+        {
+          "yaw": -0.6880279851521092,
+          "pitch": 0.36533087973573153,
+          "rotation": 0,
+          "target": "7-gruta-escoural--setup-009"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-gruta-escoural--setup-011",
+      "name": "Gruta Escoural- Setup 011",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7772470533205826,
+        "pitch": 0.11140084093050717,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9594283835280049,
+          "pitch": 0.5334782409038716,
+          "rotation": 0,
+          "target": "10-gruta-escoural--setup-013"
+        },
+        {
+          "yaw": -0.6602460431767572,
+          "pitch": 0.609914565169861,
+          "rotation": 0,
+          "target": "8-gruta-escoural--setup-010"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-gruta-escoural--setup-013",
+      "name": "Gruta Escoural- Setup 013",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8823845169973605,
+        "pitch": 0.4064985204742868,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0589652051822984,
+          "pitch": 0.5548635456374935,
+          "rotation": 0,
+          "target": "9-gruta-escoural--setup-011"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-gruta-escoural--setup-016",
+      "name": "Gruta Escoural- Setup 016",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.7851720112429916,
+        "pitch": 0.39695365902417024,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3806804850794716,
+          "pitch": 0.36304522688410046,
+          "rotation": 0,
+          "target": "7-gruta-escoural--setup-009"
+        },
+        {
+          "yaw": -0.8481354953499327,
+          "pitch": 0.5775138969191076,
+          "rotation": 0,
+          "target": "12-gruta-escoural--setup-017"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-gruta-escoural--setup-017",
+      "name": "Gruta Escoural- Setup 017",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.7897832201073243,
+        "pitch": 0.4559897637735233,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20488937279252006,
+          "pitch": 0.2930387704431858,
+          "rotation": 0,
+          "target": "11-gruta-escoural--setup-016"
+        },
+        {
+          "yaw": -2.7161233964049103,
+          "pitch": 0.5612375813728754,
+          "rotation": 0,
+          "target": "13-gruta-escoural--setup-018"
+        },
+        {
+          "yaw": 1.7728160584589903,
+          "pitch": 0.5534517432297363,
+          "rotation": 0,
+          "target": "15-gruta-escoural--setup-020"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-gruta-escoural--setup-018",
+      "name": "Gruta Escoural- Setup 018",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.4114616969278213,
+        "pitch": 0.4406817055932031,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8949714763093368,
+          "pitch": 0.004681036341446898,
+          "rotation": 0,
+          "target": "12-gruta-escoural--setup-017"
+        },
+        {
+          "yaw": -2.268475672470533,
+          "pitch": 0.5864337418881185,
+          "rotation": 0,
+          "target": "14-gruta-escoural--setup-019"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-gruta-escoural--setup-019",
+      "name": "Gruta Escoural- Setup 019",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3411999368269303,
+        "pitch": 0.4318739401201075,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3761607535435658,
+          "pitch": 0.5387795969384435,
+          "rotation": 0,
+          "target": "13-gruta-escoural--setup-018"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-gruta-escoural--setup-020",
+      "name": "Gruta Escoural- Setup 020",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.6487480596880424,
+        "pitch": 0.6691236288324198,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1052764911484196,
+          "pitch": 0.3485480353348951,
+          "rotation": 0,
+          "target": "12-gruta-escoural--setup-017"
+        },
+        {
+          "yaw": -2.7731346729387187,
+          "pitch": 0.7151102691560762,
+          "rotation": 0,
+          "target": "16-gruta-escoural--setup-022"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-gruta-escoural--setup-022",
+      "name": "Gruta Escoural- Setup 022",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.3963590644249457,
+        "pitch": -0.17823692282871662,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9016845363362567,
+          "pitch": -0.24999950356007616,
+          "rotation": 0,
+          "target": "15-gruta-escoural--setup-020"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
